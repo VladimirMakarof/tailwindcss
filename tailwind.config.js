0287007fbde0027a20d2ac371cc26536f1ac8f0a
VladimirMakarof <stan13.09@mail.ru>
2023-02-23 18:49:14 +0300

    feat: add files

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      gridTemplateRows: {
+        'layout': 'auto_1fr_auto',
+      },
+      colors: {
+        'logo-main': '#9b0404',
+      },
+      fontFamily: {
+        main: ['Montserrat'],
+
+      }
+    },
+  },
+  plugins: [],
+}
